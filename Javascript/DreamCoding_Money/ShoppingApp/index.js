8e201bf42a7082e088d9479de2df5a1f569187cb
WooCheol <jikimee64@gmail.com>
2021-08-08 17:47:14 +0900

    [Feat] 드림코딩 ShoppingApp webpack-dev-server 세팅
    1.webpack 버전 5 기준

--- Javascript/DreamCoding_Money/ShoppingApp/index.js
+++ Javascript/DreamCoding_Money/ShoppingApp/index.js
@@ -3,4 +3,19 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+import apiCall from './apiCall';
+
+window.addEventListener("load", () => {
+    // toDo = new Todo();
+});
+
+
+class Todo{
+
+    constructor() {
+        // querySelect 모음집
+
+        //addEventListener 모음집
+    }
+}
 
--- Javascript/DreamCoding_Money/ShoppingApp/webpack.config.js
+++ Javascript/DreamCoding_Money/ShoppingApp/webpack.config.js
@@ -1,4 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const AddAsset = require('add-asset-html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -16,6 +18,23 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        contentBase : __dirname + "/dist", //브라우저에 제공할 정적파일들이 담긴 경로
+        inline : true,
+        hot : true,
+        host : 'localhost',
+        compress: true, //모든 항목에 대해 gzip을 사용할 것인가.
+        port: 9000
+        // historyApiFallback: true,
+        // port: 9000,
+        // publicPath: '/dist/',
+        // proxy: {
+        //     '/api/': {
+        //         target: '백엔드주소',
+        //         changeOrigin: true,
+        //     }
+        // }
+    },
     plugins: [
         /**
          * All files inside webpack's output.path directory will be removed once, but the
@@ -29,5 +48,7 @@ module.exports = {
          * See `Options and Defaults` for information
          */
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: 'index.html' }),
+        new AddAsset({ filepath: 'style.css' })
     ],
 };
